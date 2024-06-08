5fc0098cf19d3167c2adbc00d232902ccbd15d09
rafael <155000051+1-rafael-1@users.noreply.github.com>
2024-06-09 01:01:01 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_rp::gpio;
+use embassy_time::{Duration, Timer};
+use gpio::{Level, Output};
+use {defmt_rtt as _, panic_probe as _};
+
+#[cortex_m_rt::pre_init]
+unsafe fn before_main() {
+    // Soft-reset doesn't clear spinlocks. Clear the one used by critical-section
+    // before we hit main to avoid deadlocks when using a debugger
+    embassy_rp::pac::SIO.spinlock(31).write_value(1);
+}
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    info!("Program start");
+    let p = embassy_rp::init(Default::default());
+    let mut led = Output::new(p.PIN_25, Level::Low);
+
+    loop {
+        info!("led on!");
+        led.set_high();
+        Timer::after(Duration::from_secs(1)).await;
+
+        info!("led off!");
+        led.set_low();
+        Timer::after(Duration::from_secs(1)).await;
+    }
+}
