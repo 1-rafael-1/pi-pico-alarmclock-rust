9c1da31c032ced6bc9d258f62f915bdf53b0794c
rafael <rafael>
2024-06-09 11:51:43 +0200

    save

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,22 @@ use embassy_time::{Duration, Timer};
 use gpio::{Level, Output};
 use {defmt_rtt as _, panic_probe as _};
 
-#[cortex_m_rt::pre_init]
-unsafe fn before_main() {
-    // Soft-reset doesn't clear spinlocks. Clear the one used by critical-section
-    // before we hit main to avoid deadlocks when using a debugger
-    embassy_rp::pac::SIO.spinlock(31).write_value(1);
+mod inits {
+    // TODO: was in the template, not sure if needed
+    #[cortex_m_rt::pre_init]
+    unsafe fn before_main() {
+        // Soft-reset doesn't clear spinlocks. Clear the one used by critical-section
+        // before we hit main to avoid deadlocks when using a debugger
+        embassy_rp::pac::SIO.spinlock(31).write_value(1);
+    }
 }
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     info!("Program start");
-    let p = embassy_rp::init(Default::default());
-    let mut led = Output::new(p.PIN_25, Level::Low);
+
+    let peripherals = embassy_rp::init(Default::default());
+    let mut led = Output::new(peripherals.PIN_25, Level::Low);
 
     loop {
         info!("led on!");
