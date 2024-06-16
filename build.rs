a54a8264aa9df8ab601001a65ae2e1eba8413709
rafael <155000051+1-rafael-1@users.noreply.github.com>
2024-06-16 16:08:44 +0200

    4-add-wifi-connection (#5)
    * save
    
    * Update rust-toolchain.toml to use stable channel
    
    * safely use wifi credentials via build.rs and include! in wifi_mgr
    
    * better orga and errors when reading wifi secrets
    
    * wifi connects
    
    * build fails: missing wifi config file, need a dummy file for ci
    
    ---------
    
    Co-authored-by: rafael <rafael>

--- build.rs
+++ build.rs
@@ -8,12 +8,60 @@
 //! updating `memory.x` ensures a rebuild of the application with the
 //! new memory settings.
 
+use serde_json;
 use std::env;
+use std::fs;
 use std::fs::File;
+use std::io;
 use std::io::Write;
+use std::path::Path;
 use std::path::PathBuf;
 
 fn main() {
+    println!("in build.rs");
+    memory_x();
+    wifi_secrets().unwrap();
+}
+
+fn wifi_secrets() -> io::Result<()> {
+    println!("in wifi_secrets");
+    // Read the wifi_config.json file and write the SSID and password to wifi_secrets.rs
+
+    // Create a new file in the output directory
+    let out_dir = env::var("OUT_DIR").expect("OUT_DIR environment variable not set");
+    let dest_path = Path::new(&out_dir).join("wifi_secrets.rs");
+    let mut f = File::create(&dest_path).expect("Could not create wifi_secrets.rs file");
+
+    // Read the wifi_config.json file, or create it with dummy values if it doesn't exist
+    let config_path = Path::new("wifi_config.json");
+    let config_contents = if config_path.exists() {
+        fs::read_to_string(config_path).expect("Could not read wifi_config.json file")
+    } else {
+        println!("wifi_config.json not found, creating with dummy values");
+        let dummy_config = r#"{"ssid":"dummy","password":"dummy"}"#;
+        fs::write(config_path, dummy_config).expect("Could not write dummy wifi_config.json file");
+        dummy_config.to_string()
+    };
+
+    // Parse the JSON and extract the SSID and password
+    let config: serde_json::Value =
+        serde_json::from_str(&config_contents).expect("Could not parse wifi_config.json file");
+    let ssid = config["ssid"]
+        .as_str()
+        .expect("ssid not found in wifi_config.json file");
+    let password = config["password"]
+        .as_str()
+        .expect("password not found in wifi_config.json file");
+
+    // Write the SSID and password to wifi_secrets.rs
+    println!("in wifi_secrets, before writing ssid and password to output file");
+    writeln!(f, "pub const SSID: &str = {:?};", ssid)?;
+    writeln!(f, "pub const PASSWORD: &str = {:?};", password)?;
+    Ok(())
+}
+
+fn memory_x() {
+    print!("in memory_x");
     // Put `memory.x` in our output directory and ensure it's
     // on the linker search path.
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
---
+++ src/classes/wifi_mgr.rs
@@ -0,0 +1,144 @@
+#![allow(async_fn_in_trait)]
+//make sure to have a wifi_manager.json file in the classes folder formatted as follows:
+//{
+//    "ssid": "some_ssid_here",
+//    "password": "some_password_here"
+//}
+//also make sure that build.rs loads the wifi_manager.json file and writes it to wifi_secrets.rs
+include!(concat!(env!("OUT_DIR"), "/wifi_secrets.rs"));
+
+//use core::str;
+
+//use crate::classes::irqs::Irqs;
+use cyw43::ControlError;
+use cyw43_pio::PioSpi;
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_net::Stack;
+//use embassy_rp::bind_interrupts;
+use embassy_rp::gpio::Output;
+use embassy_rp::peripherals::{DMA_CH0, PIO0};
+//use embassy_rp::pio::{InterruptHandler, Pio};
+use embassy_time::{Duration, Timer};
+use heapless::String;
+use static_cell::StaticCell;
+use {defmt_rtt as _, panic_probe as _};
+
+enum WifiState {
+    Disconnected,
+    Connected,
+    Error(ControlError), // Optionally, include an error message
+}
+
+struct WifiManager {
+    state: WifiState,
+    ssid: Option<String<128>>,
+    password: Option<String<128>>,
+}
+
+impl WifiManager {
+    fn new() -> Self {
+        WifiManager {
+            state: WifiState::Disconnected,
+            ssid: None,
+            password: None,
+        }
+    }
+
+    fn set_state(&mut self, new_state: WifiState) {
+        self.state = new_state;
+    }
+
+    fn get_state(&self) -> &WifiState {
+        &self.state
+    }
+
+    fn set_credentials(&mut self) {
+        self.ssid = Some(self.convert_str_to_heapless_safe(SSID).unwrap());
+        self.password = Some(self.convert_str_to_heapless_safe(PASSWORD).unwrap());
+    }
+
+    fn convert_str_to_heapless_safe(
+        &mut self,
+        s: &str,
+    ) -> Result<heapless::String<128>, &'static str> {
+        let mut heapless_string: heapless::String<128> = heapless::String::new();
+        for c in s.chars() {
+            if heapless_string.push(c).is_err() {
+                return Err("String exceeds capacity");
+            }
+        }
+        Ok(heapless_string)
+    }
+
+    fn convert_heapless_safe_to_str(
+        &self,
+        s: Option<heapless::String<128>>,
+    ) -> heapless::String<128> {
+        match s {
+            Some(value) => value,            // Directly return the heapless::String<128>
+            None => heapless::String::new(), // Return an empty heapless::String if None
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn wifi_task(
+    runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>,
+) -> ! {
+    runner.run().await
+}
+
+#[embassy_executor::task]
+async fn net_task(stack: &'static Stack<cyw43::NetDriver<'static>>) -> ! {
+    stack.run().await
+}
+
+#[embassy_executor::task]
+pub async fn connect_wifi(
+    spawner: Spawner,
+    pwr: Output<'static>,
+    spi: PioSpi<'static, PIO0, 0, DMA_CH0>,
+) {
+    let mut wifi_manager = WifiManager::new(); // Initialize WifiManager
+    wifi_manager.set_credentials(); // Set credentials from wifi_secrets.rs
+
+    // let fw = include_bytes!("../../../../cyw43-firmware/43439A0.bin");
+    // let clm = include_bytes!("../../../../cyw43-firmware/43439A0_clm.bin");
+    // To make flashing faster for development, you may want to flash the firmwares independently
+    // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
+    //     probe-rs download 43439A0.bin --binary-format bin --chip RP2040 --base-address 0x10100000
+    //     probe-rs download 43439A0_clm.bin --binary-format bin --chip RP2040 --base-address 0x10140000
+    let fw = unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, 230321) };
+    let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
+
+    static STATE: StaticCell<cyw43::State> = StaticCell::new();
+    let state = STATE.init(cyw43::State::new());
+    let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    unwrap!(spawner.spawn(wifi_task(runner)));
+
+    control.init(clm).await;
+    control
+        .set_power_management(cyw43::PowerManagementMode::PowerSave)
+        .await;
+
+    let ssid_str = wifi_manager.convert_heapless_safe_to_str(wifi_manager.ssid.clone()); // Assuming ssid is Option<heapless::String<128>>
+    let password_str = wifi_manager.convert_heapless_safe_to_str(wifi_manager.password.clone()); // Assuming password is Option<heapless::String<128>>
+
+    info!(
+        "Joining WPA2 network with SSID: {:?} and password: {:?}",
+        ssid_str, password_str
+    );
+
+    if let Err(e) = control.join_wpa2(&ssid_str, &password_str).await {
+        wifi_manager.set_state(WifiState::Error(e));
+    } else {
+        wifi_manager.set_state(WifiState::Connected);
+    };
+
+    // every 120 seconds, check if we are still connected
+    loop {
+        Timer::after(Duration::from_secs(120)).await;
+        info!("In loop and doing fuck all");
+    }
+}
