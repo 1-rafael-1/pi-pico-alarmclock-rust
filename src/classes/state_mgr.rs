578e38ed3df3a728cf12a4f1c917d9b3d2ebaf5f
rafael <rafael>
2024-06-16 17:16:56 +0200

    StateManager comments

--- src/classes/state_mgr.rs
+++ src/classes/state_mgr.rs
@@ -1,3 +1,9 @@
+// not sure if this will even be a thing... but it's here for now
+// This is a class that will be used to manage the state of the system, I will need something to call functions across the system and that gets messy quickly
+// This will be a way to keep things organized
+// Actually I guess I need some sort of event system to call functions across the system... I'll have to look into that but Rust does not seem to have a built in event system
+// For now the idea is to do a small version of dependenbcy injection and pass this class around to the other classes that need it; none of that yet implemented
+
 use defmt::*;
 
 pub trait StateManagement {
