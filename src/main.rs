0ba0ff22a074a4c2d68a369ea30ac6d12d0874c2
rafael <rafael>
2024-06-26 20:47:31 +0200

    use p instead of peripherals

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,21 @@
 #![no_std]
 #![no_main]
 
+use core::cell::RefCell;
+
 use crate::classes::time_updater::TimeUpdater;
 use cyw43_pio::PioSpi; // for WiFi
 use defmt::*; // global logger
 use embassy_executor::Spawner; // executor
-use embassy_rp::bind_interrupts;
 use embassy_rp::gpio::{self, Input};
 use embassy_rp::peripherals::PIO0;
 use embassy_rp::pio::{InterruptHandler, Pio};
 use embassy_rp::rtc::Rtc;
+use embassy_rp::{bind_interrupts, peripherals};
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
 use embassy_time::{Duration, Timer}; // time
-use gpio::{Level, Output}; // gpio output
+use gpio::{Level, Output};
+use static_cell::StaticCell; // gpio output
 use {defmt_rtt as _, panic_probe as _}; // panic handler
 
 // import the classes module (submodule of src)
@@ -27,29 +31,26 @@ async fn main(spawner: Spawner) {
     info!("Program start");
 
     // Initialize the peripherals for the RP2040
-    let peripherals = embassy_rp::init(Default::default());
-
-    // Initialize one LED on pin 25
-    // let mut led = Output::new(peripherals.PIN_25, Level::Low);
+    let p = embassy_rp::init(Default::default());
 
     // buttons
     // green_button
     info!("init green button");
-    let green_button_input = Input::new(peripherals.PIN_20, gpio::Pull::Up);
+    let green_button_input = Input::new(p.PIN_20, gpio::Pull::Up);
     spawner
         .spawn(classes::btn_mgr::green_button(spawner, green_button_input))
         .unwrap();
 
     //blue_button
     info!("init blue button");
-    let blue_button_input = Input::new(peripherals.PIN_21, gpio::Pull::Up);
+    let blue_button_input = Input::new(p.PIN_21, gpio::Pull::Up);
     spawner
         .spawn(classes::btn_mgr::blue_button(spawner, blue_button_input))
         .unwrap();
 
     //yellow_button
     info!("init yellow button");
-    let yellow_button_input = Input::new(peripherals.PIN_22, gpio::Pull::Up);
+    let yellow_button_input = Input::new(p.PIN_22, gpio::Pull::Up);
     spawner
         .spawn(classes::btn_mgr::yellow_button(
             spawner,
@@ -60,19 +61,19 @@ async fn main(spawner: Spawner) {
     // Real Time Clock
     // Setup for WiFi connection and RTC update
     info!("init wifi");
-    let pwr = Output::new(peripherals.PIN_23, Level::Low);
-    let cs = Output::new(peripherals.PIN_25, Level::High);
-    let mut pio = Pio::new(peripherals.PIO0, Irqs);
+    let pwr = Output::new(p.PIN_23, Level::Low);
+    let cs = Output::new(p.PIN_25, Level::High);
+    let mut pio = Pio::new(p.PIO0, Irqs);
     let spi = PioSpi::new(
         &mut pio.common,
         pio.sm0,
         pio.irq0,
         cs,
-        peripherals.PIN_24,
-        peripherals.PIN_29,
-        peripherals.DMA_CH0,
+        p.PIN_24,
+        p.PIN_29,
+        p.DMA_CH0,
     );
-    let rtc = Rtc::new(peripherals.RTC);
+
     bind_interrupts!(struct Irqs {
         PIO0_IRQ_0 => InterruptHandler<PIO0>;
     });
@@ -87,19 +88,11 @@ async fn main(spawner: Spawner) {
             time_updater,
             pwr,
             spi,
-            rtc,
         ))
         .unwrap();
 
     loop {
         info!("main loop");
         Timer::after(Duration::from_secs(10)).await;
-        // info!("led on!");
-        // led.set_high();
-        // Timer::after(Duration::from_secs(20)).await;
-
-        // info!("led off!");
-        // led.set_low();
-        // Timer::after(Duration::from_secs(20)).await;
     }
 }
