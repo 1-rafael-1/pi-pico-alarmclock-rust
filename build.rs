fe9d99841b88e6211b64c6c4fea3eae4f470dc19
rafael <rafael.koch@gmx.net>
2024-07-24 18:03:44 +0200

    33-complete-the-system-states (#35)

--- build.rs
+++ build.rs
@@ -35,7 +35,7 @@ fn wifi_secrets() -> io::Result<()> {
     // Read the wifi_config.json file, or create it with dummy values if it doesn't exist
     let manifest_dir =
         env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR environment variable not set");
-    let config_path = Path::new(&manifest_dir).join("src/config/wifi_config.json");
+    let config_path = Path::new(&manifest_dir).join("config/wifi_config.json");
     //let config_path = Path::new("src/config/wifi_config.json");
     let config_contents = if config_path.exists() {
         fs::read_to_string(config_path).expect("Could not read wifi_config.json file")
@@ -75,7 +75,7 @@ fn time_api_config() -> io::Result<()> {
     // Read the time_api.json file, or create it with dummy values if it doesn't exist
     let manifest_dir =
         env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR environment variable not set");
-    let config_path = Path::new(&manifest_dir).join("src/config/time_api.json");
+    let config_path = Path::new(&manifest_dir).join("config/time_api.json");
     //let config_path = Path::new("src/config/time_api.json");
     let config_contents = if config_path.exists() {
         fs::read_to_string(config_path).expect("Could not read time_api.json file")
