c6cc240d5b4421704ea70a88ac04e5084b5571a3
rafael <rafael.koch@gmx.net>
2024-08-17 16:47:13 +0200

    held button sends multiple events down the channel; remove presses which was unused (#54)

--- src/task/buttons.rs
+++ src/task/buttons.rs
@@ -3,8 +3,7 @@
 
 use crate::task::resources::{BlueButtonResources, GreenButtonResources, YellowButtonResources};
 use crate::task::task_messages::{Events, EVENT_CHANNEL};
-use defmt::info;
-use defmt::Format;
+use defmt::{info, Format};
 use embassy_executor::Spawner;
 use embassy_rp::gpio::{self, Input, Level};
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
@@ -20,6 +19,7 @@ pub struct ButtonManager<'a> {
     debounce_duration: Duration,
     events: Events,
     button: Button,
+    hold_event_interval: Duration,
     sender: Sender<'a, CriticalSectionRawMutex, Events, 10>,
 }
 
@@ -41,61 +41,92 @@ impl<'a> ButtonManager<'a> {
     ) -> Self {
         Self {
             input,
-            debounce_duration: Duration::from_millis(100), // hardcoding, all buttons have the same debounce duration
+            debounce_duration: Duration::from_millis(80), // hardcoding, all buttons have the same debounce duration
             events,
             button,
             sender,
+            hold_event_interval: Duration::from_millis(150), // hardcoding, all buttons have the same hold event interval
         }
     }
 
+    /// Handle the button press event. This function is an infinite loop that waits for a debounced button press event, then determines if the button was pressed or held.
+    /// The most important thing to know here is that a basic button event is either the button being pressed or being released, both of which are a change in the input level that we track.
     pub async fn handle_button_press(&mut self) {
-        loop {
-            // button pressed
-            let _debounce = self.debounce().await;
-            let start = Instant::now();
-            info!("{} Press", self.events);
+        'mainloop: loop {
+            // we do nothing, until we have a debounced button event, either changing from high to low or low to high. Here at this point we expect the level to be low, normally.
+            // The button is normally high, and when pressed, it goes low. So we wait for the button to be pressed.
 
-            // send button press to channel -> this is a ToDo, we will want to do this reacting to the length of the press somehow
-            // maybe we need a pipe instead of a channel to stream the button presses to the orchestrator
-            let presses: u32 = 0;
-            let event = match self.events {
-                Events::BlueBtn(0) => Events::BlueBtn(presses + 1),
-                Events::GreenBtn(0) => Events::GreenBtn(presses + 1),
-                Events::YellowBtn(0) => Events::YellowBtn(presses + 1),
-                _ => panic!("Invalid Event"),
+            let init_level = self.debounce().await;
+            // if the button is not pressed, we continue with the main loop
+            if init_level != Level::Low {
+                continue 'mainloop;
             };
-            self.sender.send(event).await;
 
-            match with_deadline(start + Duration::from_secs(1), self.debounce()).await {
-                // Button Released < 1s
-                Ok(_) => {
-                    info!("pressed for: {}ms", start.elapsed().as_millis());
-                    continue;
+            // we wait for the button to be released, depending on how fast that happens, we have a one-time press event or a hold.
+            let level_result =
+                with_deadline(Instant::now() + Duration::from_secs(1), self.debounce()).await;
+            match level_result {
+                // Button Released < 1s -> we have a one-time press event
+                Ok(level) => {
+                    // if the button is not released, we continue with the main loop
+                    if level != Level::High {
+                        continue 'mainloop;
+                    } else {
+                        // we send one press event down the channel
+                        let event = match self.events {
+                            Events::BlueBtn => Events::BlueBtn,
+                            Events::GreenBtn => Events::GreenBtn,
+                            Events::YellowBtn => Events::YellowBtn,
+                            _ => panic!("Invalid Event"),
+                        };
+                        self.sender.send(event).await;
+                        // and then we continue with the main loop
+                        continue 'mainloop;
+                    }
                 }
                 // button held for > 1s
+                // not a one-time press event, but a hold event
                 Err(_) => {
-                    info!("Held");
+                    // here we do nothing and leave this pattern matching block
+                }
+            };
+
+            // we have a button being held, we need to handle the hold event.
+            'holding: loop {
+                // we wait for either the button to change its level or the hold event interval to expire
+                let level_result = with_deadline(
+                    Instant::now() + self.hold_event_interval,
+                    self.input.wait_for_any_edge(),
+                )
+                .await;
+                match level_result {
+                    Ok(_) => {
+                        // if the button level changed, we break the loop and continue with the main loop and send no event
+                        break 'holding;
+                    }
+                    Err(_) => {
+                        if self.input.get_level() == Level::High {
+                            // if the button is released, we continue with the main loop and send no event
+                            continue 'mainloop;
+                        } else {
+                            // if the button is still held, we send an event down the channel, and then return to the beginning of the loop
+                            let event = match self.events {
+                                Events::BlueBtn => Events::BlueBtn,
+                                Events::GreenBtn => Events::GreenBtn,
+                                Events::YellowBtn => Events::YellowBtn,
+                                _ => panic!("Invalid Event"),
+                            };
+                            self.sender.send(event).await;
+                            continue 'holding;
+                        }
+                    }
                 }
             }
-
-            match with_deadline(start + Duration::from_secs(5), self.debounce()).await {
-                // Button released <5s
-                Ok(_) => {
-                    info!("pressed for: {}ms", start.elapsed().as_millis());
-                    continue;
-                }
-                // button held for > >5s
-                Err(_) => {
-                    info!("Long Held");
-                }
-            }
-
-            // wait for button release before handling another press
-            self.debounce().await;
-            info!("pressed for: {}ms", start.elapsed().as_millis());
         }
     }
 
+    /// Debounce the button press by waiting for the button to be stable for a given duration. We determine the input level, then await any edge,
+    /// then wait for the debounce duration, then check if the input level has changed. If it has, we break the loop and return the new level.
     pub async fn debounce(&mut self) -> Level {
         loop {
             let l1 = self.input.get_level();
@@ -116,7 +147,7 @@ impl<'a> ButtonManager<'a> {
 pub async fn green_button_handler(_spawner: Spawner, r: GreenButtonResources) {
     let input = gpio::Input::new(r.button_pin, gpio::Pull::Up);
     let sender = EVENT_CHANNEL.sender();
-    let mut btn = ButtonManager::new(input, Events::GreenBtn(0), Button::Green, sender);
+    let mut btn = ButtonManager::new(input, Events::GreenBtn, Button::Green, sender);
     info!("{} task started", btn.button);
     btn.handle_button_press().await;
 }
@@ -125,7 +156,7 @@ pub async fn green_button_handler(_spawner: Spawner, r: GreenButtonResources) {
 pub async fn blue_button_handler(_spawner: Spawner, r: BlueButtonResources) {
     let input = gpio::Input::new(r.button_pin, gpio::Pull::Up);
     let sender = EVENT_CHANNEL.sender();
-    let mut btn = ButtonManager::new(input, Events::BlueBtn(0), Button::Blue, sender);
+    let mut btn = ButtonManager::new(input, Events::BlueBtn, Button::Blue, sender);
     info!("{} task started", btn.button);
     btn.handle_button_press().await;
 }
@@ -134,7 +165,7 @@ pub async fn blue_button_handler(_spawner: Spawner, r: BlueButtonResources) {
 pub async fn yellow_button_handler(_spawner: Spawner, r: YellowButtonResources) {
     let input = gpio::Input::new(r.button_pin, gpio::Pull::Up);
     let sender = EVENT_CHANNEL.sender();
-    let mut btn = ButtonManager::new(input, Events::YellowBtn(0), Button::Yellow, sender);
+    let mut btn = ButtonManager::new(input, Events::YellowBtn, Button::Yellow, sender);
     info!("{} task started", btn.button);
     btn.handle_button_press().await;
 }
